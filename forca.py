fe74e5431ab5a200140d6a0679a772a95b2e3610
CODantas <114962516+CODantas@users.noreply.github.com>
2023-04-05 21:07:53 -0300

    foi criado um array

--- forca.py
+++ forca.py
@@ -3,7 +3,30 @@ def jogar():
     print("***Bem vindo ao jogo da Forca!***")
     print("*********************************")
 
+    palavra_secreta = "banana"
+    letras_acertadas = ['_','_','_','_','_','_']
+
+    enforcou = False
+    acertou = False
+
+    print(letras_acertadas)
+
+    #enquanto
+    while(not enforcou and not acertou):
+
+        chute = input("Qual a letra?")
+        chute = chute.strip()
+
+        index = 0
+        for letra in palavra_secreta:
+            if(chute.upper() == letra.upper()):
+                letras_acertadas[index] = letra
+            index += 1
+
+        print("jogando ...")
+
     print("Fim do jogo")
+
 
 if(__name__ == "__main__"):
     jogar()
